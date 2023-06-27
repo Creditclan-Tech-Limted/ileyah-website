80a2c10b8c6b85c19e553eea37b6fbc8c9e7f5ad
Oyegbile Praise <oluwadhammieh@gmail.com>
2023-06-27 15:43:19 +0100

    feat: Added information source to foundhouse flow

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,15 @@ import LandingPage from "@/views/LandingPage";
 import WhatsappSlideOut from "@/components/WhatsappSlideOut";
 import useGlobalStore from "@/store/global";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { useEffect } from "react";
-import TagManager from "react-gtm-module";
+// import { useEffect } from "react";
+// import TagManager from "react-gtm-module";
 
 
 export default function Home() {
-  useEffect(() => {
-    console.log('enter');
-    TagManager.initialize({ gtmId: 'GTM-5DKKR2P' });
-  }, [])
+  // useEffect(() => {
+  //   console.log('enter');
+  //   TagManager.initialize({ gtmId: 'GTM-5DKKR2P' });
+  // }, [])
 
   const queryClient = new QueryClient()
 
