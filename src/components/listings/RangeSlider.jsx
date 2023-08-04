e5275ca48bfb5b21307ec49d29caa4ae69caa698
osomhe1 <osomhe.leogho@gmail.com>
2023-08-04 02:08:59 +0100

    range slider

--- src/components/listings/RangeSlider.jsx
+++ src/components/listings/RangeSlider.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import InputRange from 'react-input-range'
 import 'react-input-range/lib/css/index.css'
 
-const RangeSlider = ({title, minAmount, maxAmount}) => {
+const PriceRangeSlider = ({ title, minAmount, maxAmount }) => {
   const [value, setValue] = useState({ min: minAmount, max: maxAmount })
 
   const handleRangeChange = (newValue) => {
@@ -10,24 +10,23 @@ const RangeSlider = ({title, minAmount, maxAmount}) => {
   }
 
   return (
-    <div className='container mx-auto p-4 '>
-      <div className='border-b-2'>
-
-      <h1 className='font-bold text-xl mb-4'> {title} </h1>
-      <InputRange
-        minValue={minAmount}
-        maxValue={maxAmount}
-        value={value}
-        onChange={handleRangeChange}
-        // formatLabel={(value) => `${value}`}
-      />
-      {/* <div className='flex justify-between mt-4'>
-        <span>{value.min}%</span>
-        <span>{value.max}%</span>
-      </div> */}
+    <div className='container mx-auto p-4'>
+      <div className='border-b-2 p-4'>
+        <h1 className='font-bold text-xl mb-4'>{title}</h1>
+        <InputRange
+          minValue={minAmount}
+          maxValue={maxAmount}
+          value={value}
+          onChange={handleRangeChange}
+          formatLabel={(value) => `$${value}`}
+        />
+        <div className='flex justify-between mt-4'>
+          <span>${value.min}</span>
+          <span>${value.max}</span>
+        </div>
       </div>
     </div>
   )
 }
 
-export default RangeSlider
+export default PriceRangeSlider
