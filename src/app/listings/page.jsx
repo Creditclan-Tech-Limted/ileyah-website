d0a1bb4cbb69ccf60659aaf539a83ced4865e7c9
osomhe1 <osomhe.leogho@gmail.com>
2023-08-14 16:55:13 +0100

    conflct resolve

--- src/app/listings/page.jsx
+++ src/app/listings/page.jsx
@@ -13,6 +13,7 @@ import ScrollToTop from '@/components/ScrollToTop'
 import ScrollToTopBtn from '@/components/ScrollToTpBtn'
 import { IconLayoutGrid, IconLayoutList } from '@tabler/icons-react'
 import Footer from '@/components/Footer'
+import Link from 'next/link'
 
 const image1 = `https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80`
 const imageAvatar = `https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60`
@@ -98,45 +99,51 @@ const Page = () => {
           {isGridView ? (
             <div className='grid grid-cols-2 gap-4'>
               {[0, 1, 2, 3].map((m, i) => (
-                <ListingsGrid
-                  key={i}
-                  houseImg={image1}
-                  heading='For Rent'
-                  price='$34,900/Month'
-                  title='New Apartment Nice View'
-                  avatar={imageAvatar}
-                  name='Jonathan Reinink'
-                  role='Estate Agents'
-                  location=' Belmore Garden, Chicago'
-                  lengthNum='3450'
-                  bedNum='3'
-                  bathNum='2'
-                  bed='Bed'
-                  bath='Bath'
-                  length='Square Ft'
-                />
+                <Link key={i}  href='/listings/page_details'>
+                  <ListingsGrid
+                    key={i}
+                    houseImg={image1}
+                    heading='For Rent'
+                    price='$34,900/Month'
+                    title='New Apartment Nice View'
+                    avatar={imageAvatar}
+                    name='Jonathan Reinink'
+                    role='Estate Agents'
+                    location=' Belmore Garden, Chicago'
+                    lengthNum='3450'
+                    bedNum='3'
+                    bathNum='2'
+                    bed='Bed'
+                    bath='Bath'
+                    length='Square Ft'
+                  />
+                </Link>
+
               ))}
             </div>
           ) : (
             <div className=''>
               {[0, 1, 2, 3].map((m, i) => (
-                <ListingFlex
-                  key={i}
-                  houseImg={image1}
-                  heading='For Rent'
-                  price='$34,900/Month'
-                  title='New Apartment Nice View'
-                  avatar={imageAvatar}
-                  name='Jonathan Reinink'
-                  role='Estate Agents'
-                  location=' Belmore Garden, Chicago'
-                  lengthNum='3450'
-                  bedNum='3'
-                  bathNum='2'
-                  bed='Bed'
-                  bath='Bath'
-                  length='Square Ft'
-                />
+                <Link key={i}  href='/listings/page_details'>
+                  <ListingFlex
+                    key={i}
+                    houseImg={image1}
+                    heading='For Rent'
+                    price='$34,900/Month'
+                    title='New Apartment Nice View'
+                    avatar={imageAvatar}
+                    name='Jonathan Reinink'
+                    role='Estate Agents'
+                    location=' Belmore Garden, Chicago'
+                    lengthNum='3450'
+                    bedNum='3'
+                    bathNum='2'
+                    bed='Bed'
+                    bath='Bath'
+                    length='Square Ft'
+                  />
+                </Link>
+                
               ))}
             </div>
           )}
