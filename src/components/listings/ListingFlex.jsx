3338b3615f36991803e0f34a9cca0569ade0efda
osomhe1 <osomhe.leogho@gmail.com>
2023-08-18 15:18:27 +0100

    ui review

--- src/components/listings/ListingFlex.jsx
+++ src/components/listings/ListingFlex.jsx
@@ -3,7 +3,7 @@ import { IconMapPinFilled } from '@tabler/icons-react'
 const ListingFlex = ({heading, price, title, houseImg, location, name,lengthNum, bedNum, bathNum, bed, bath, length, role, avatar}) => {
   return (
     <div>
-      <div className='max-w-sm w-full bg-white lg:max-w-4xl lg:flex lg:gap-4 p-8 shadow m-auto mt-4'>
+      <div className='max-w-sm w-full relative bg-white lg:max-w-4xl lg:flex lg:gap-4 p-8 shadow m-auto mt-4'>
         <div
           className='h-48 lg:h-64 lg:w-80 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden hover:scale-110 transition duration-500 cursor-pointer'
           style={{ backgroundImage: `url(${houseImg}) ` }}
@@ -13,17 +13,17 @@ const ListingFlex = ({heading, price, title, houseImg, location, name,lengthNum,
           <div className='mb-4'>
             <div className='flex justify-between'>
               <div className=''>
-                <p className='text-lg uppercase  text-blue-700 '>{heading}</p>
+                <p className='absolute z-[9] top-12 lg:left-12 bg-green-600 text-white rounded px-4 py-1'>
+                  {heading}
+                </p>
               </div>
-              <div className=''>
-                <p className='text-lg  text-blue-700 '>{price} </p>
-              </div>
+              
             </div>
-            <div className='text-gray-900 font-bold text-2xl mb-2 hover:text-blue-700 cursor-pointer'>
+            <div className='text-gray-900 font-bold text-2xl my-4 hover:text-blue-700 cursor-pointer'>
               {title}
             </div>
             <div className=' my-4 flex gap-2 items-center'>
-            <IconMapPinFilled size="20"/>
+              <IconMapPinFilled size='20' />
               <p className='text-gray-700 text-base hover:text-blue-700 cursor-pointer'>
                 {location}
               </p>
@@ -42,28 +42,21 @@ const ListingFlex = ({heading, price, title, houseImg, location, name,lengthNum,
               </p>
             </div>
           </div>
-          <div className='lg:flex items-center gap-8 justify-between '>
-            <div className='flex items-center'>
-              <img
-                className='w-14 h-14 border-gray-300 border-2 rounded-full mr-4'
-                src={avatar}
-                alt='Avatar of Jonathan Reinink'
-              />
-              <div className=' flex items-center justify-between '>
-                <div className='text-sm'>
-                  <p className='text-black font-bold my-2 leading-none'>
-                    {name}
-                  </p>
-                  <p className='text-gray-600'>{role}</p>
-                </div>
+          <div className='lg:flex items-center gap-4 justify-between'>
+            
+            <div className=' flex items-center justify-between '>
+              <div className='text-sm'>
+                <p className='text-black font-bold my-2 leading-none'>{name}</p>
+                <p className='text-gray-600'>{role}</p>
               </div>
             </div>
-
-          
+            <div className=''>
+              <p className='text-lg  text-blue-700 '>{price} </p>
+            </div>
           </div>
         </div>
       </div>
-      </div>
+    </div>
   )
 }
 
