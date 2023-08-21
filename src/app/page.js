de308ca70b25985baf284f0bb889f810acb1800b
Oyegbile Praise <oluwadhammieh@gmail.com>
2023-08-21 15:17:12 +0100

    conflct resolve

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,24 @@ import LandingPage from "@/views/LandingPage";
 import WhatsappSlideOut from "@/components/WhatsappSlideOut";
 import useGlobalStore from "@/store/global";
 import { QueryClient, QueryClientProvider } from "react-query";
+import mixpanel from "mixpanel-browser";
 
 export default function Home() {
   const queryClient = new QueryClient()
+
+  mixpanel.init('fa298e932b62a35082fc9c7513d81663', { debug: true, track_pageview: true, persistence: 'localStorage' });
+
+  mixpanel.identify('praise');
+
+  mixpanel.track('Sign Up', {
+    'Signup Type': 'Referral'
+  })
 
   const { isSignupOpen, toggleIsSignupOpen } = useGlobalStore(state => ({
     isSignupOpen: state.data.isSignupOpen,
     toggleIsSignupOpen: state.toggleIsSignupOpen
   }));
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
